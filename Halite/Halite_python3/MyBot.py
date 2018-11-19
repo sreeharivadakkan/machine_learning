f6bc6ca57796076c10b03853e7051856d038fddb
Sreehari Vadakkan <Sreehari.v@corp.cumulus>
2018-11-19 12:58:53 +0530

    Halite competition

---
+++ Halite/Halite_python3/MyBot.py
@@ -0,0 +1,32 @@
+import hlt
+from hlt import constants
+from hlt.positionals import Direction
+import random
+import logging
+game = hlt.Game()
+game.ready("MyPythonBot")
+logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
+
+while True:
+    
+    game.update_frame()
+    me = game.me
+    game_map = game.game_map
+    command_queue = []
+
+    for ship in me.get_ships():
+        
+        choices = ship.position.get_surrounding_cardinals()
+
+        if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
+            command_queue.append(
+                ship.move(
+                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+        else:
+            command_queue.append(ship.stay_still())
+
+    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+        command_queue.append(me.shipyard.spawn())
+
+    game.end_turn(command_queue)
+
